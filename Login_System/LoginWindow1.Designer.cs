c55f1f2ad7f9f1b4932a63e6638ad6a5c8fce782
Bryce Lagerberg <burry20@gmail.com>
2020-01-18 23:22:51 -0800

    added login UI

---
+++ Login_System/LoginWindow1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Login_System
+{
+    partial class LoginWindow1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LoginWindow = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginWindow
+            // 
+            this.LoginWindow.AutoSize = true;
+            this.LoginWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginWindow.Location = new System.Drawing.Point(12, 21);
+            this.LoginWindow.Name = "LoginWindow";
+            this.LoginWindow.Size = new System.Drawing.Size(173, 37);
+            this.LoginWindow.TabIndex = 1;
+            this.LoginWindow.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(185, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 38);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(185, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(317, 38);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 73);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Create Account";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LoginWindow1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 276);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginWindow);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "LoginWindow1";
+            this.Text = "Login Window";
+            this.Load += new System.EventHandler(this.LoginWindow1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LoginWindow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Login_System/LoginWindow1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_System
+{
+    public partial class LoginWindow1 : Form
+    {
+        public LoginWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateAccount(textBox2.Text,textBox1.Text);
+        }
+        
+        private void CreateAccount(String username, String password)
+        {
+            MessageBox.Show("Username : " + username + " Password : " + password);
+
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = "BURRYSSP6";
+            csb.InitialCatalog = "BrycesDB";
+            csb.IntegratedSecurity = true;
+
+            string connString = csb.ToString();
+
+            //Be sure to replace <YourTable> with the actual name of the Table
+            string queryString = "INSERT INTO [Login Stats](Username, Password,LoggedIn, LastLogin) VALUES ('"+username+"', '"+password+"', 0, GETDATE());";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Send these to your WinForms textboxes
+                        string nameValue = reader["Name"].ToString();
+                        string classValue = reader["Class"].ToString();
+                        string schoolValue = reader["School"].ToString();
+                    }
+                }
+            }
+        }
+
+        private void Login(String username, String password)
+        {
+
+        }
+
+        private void LoginWindow1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
