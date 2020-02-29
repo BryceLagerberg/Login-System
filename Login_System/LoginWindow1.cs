4b061be9140a82667616e272d3bd7264112c16e0
Bryce Lagerberg <burry20@gmail.com>
2020-02-29 13:43:02 -0800

    added AccountCreated to sql server, added email textbox to login window, added profile window to login button

--- Login_System/LoginWindow1.cs
+++ Login_System/LoginWindow1.cs
@@ -13,29 +13,38 @@ namespace Login_System
 {
     public partial class LoginWindow1 : Form
     {
+        private const string DataBase = "BrycesDB";
+        private const string SQLServer = "BRYCELOGERBURG\\SQLEXPRESS";
+
+        private List<ProfileData> Accounts = new List<ProfileData>();
+        private ProfileWindow PW = new ProfileWindow();
         public LoginWindow1()
         {
             InitializeComponent();
         }
 
+        // Create Account Button
         private void button2_Click(object sender, EventArgs e)
         {
-            CreateAccount(textBox2.Text,textBox1.Text);
+            CreateAccount(textBox2.Text, textBox1.Text);
         }
-        
+
+        //Create Account Function
         private void CreateAccount(String username, String password)
         {
-            MessageBox.Show("Username : " + username + " Password : " + password);
+            // a visual textbox telling username and password after creating an account
+            //MessageBox.Show("Username : " + username + " Password : " + password);
 
             SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
-            csb.DataSource = "BURRYSSP6";
-            csb.InitialCatalog = "BrycesDB";
+            csb.DataSource = SQLServer;
+            csb.InitialCatalog = DataBase;
             csb.IntegratedSecurity = true;
 
             string connString = csb.ToString();
 
             //Be sure to replace <YourTable> with the actual name of the Table
-            string queryString = "INSERT INTO [Login Stats](Username, Password,LoggedIn, LastLogin) VALUES ('"+username+"', '"+password+"', 0, GETDATE());";
+            string queryString = "INSERT INTO [Login Stats](Username, Password, LoggedIn, AccountCreated, LastLogin) VALUES ('"+username+"', '"+password+"', 0, GETDATE(), NULL);";
+            //string queryString = "select * from [Login Stats]";
 
             using (SqlConnection connection = new SqlConnection(connString))
             using (SqlCommand command = connection.CreateCommand())
@@ -43,28 +52,68 @@ namespace Login_System
                 command.CommandText = queryString;
 
                 connection.Open();
+                //command.ExecuteReader();
+
+                
+
+                
 
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
                         //Send these to your WinForms textboxes
-                        string nameValue = reader["Name"].ToString();
-                        string classValue = reader["Class"].ToString();
-                        string schoolValue = reader["School"].ToString();
+                        ProfileData PD = new ProfileData();
+                        PD.Username = reader["Username"].ToString();
+                        PD.Password = reader["Password"].ToString();
+                        PD.LoggedIn = Convert.ToBoolean(reader["LoggedIn"].ToString());
+                        Accounts.Add(PD);
                     }
                 }
+                Console.WriteLine();
+
+
+               /* foreach(ProfileData PD in Accounts)
+                {
+                    if(PD.Username == textBox1.)
+                }
+                */
             }
         }
 
+
+
+        // Login Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login(textBox1.Text, textBox2.Text);
+        }
+
+        // Login Function
         private void Login(String username, String password)
         {
-
+            PW.Visible = true;
+            this.Visible = false; 
         }
 
-        private void LoginWindow1_Load(object sender, EventArgs e)
+    }
+
+    public class ProfileData{
+        public string Username;
+        public string Password;
+        public bool LoggedIn;
+        public DateTime LastLogin;
+
+
+        public ProfileData()
         {
 
         }
+
+        public ProfileData(string username, string password, bool loggedin)
+        {
+
+        }
+
     }
 }
