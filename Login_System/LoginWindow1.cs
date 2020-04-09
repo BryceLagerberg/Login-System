af4f8c1f5ff4fdca1486d448759ded23cf1d190d
BryceLagerberg <54417214+BryceLagerberg@users.noreply.github.com>
2020-04-08 23:40:49 -0700

    Revert "Test branch (#3)" (#5)
    This reverts commit 2c3f0113438f1a79ff1c3d597cdd39f427b5b211.

--- Login_System/LoginWindow1.cs
+++ Login_System/LoginWindow1.cs
@@ -18,7 +18,6 @@ namespace Login_System
 
         private List<ProfileData> Accounts = new List<ProfileData>();
         private ProfileWindow PW = new ProfileWindow();
-        private AccountInfo AI = new AccountInfo();
         public LoginWindow1()
         {
             InitializeComponent();
@@ -27,8 +26,63 @@ namespace Login_System
         // Create Account Button
         private void button2_Click(object sender, EventArgs e)
         {
-            AI.Show();   
+            CreateAccount(textBox2.Text, textBox1.Text);
         }
+
+        //Create Account Function
+        private void CreateAccount(String username, String password)
+        {
+            // a visual textbox telling username and password after creating an account
+            //MessageBox.Show("Username : " + username + " Password : " + password);
+
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = SQLServer;
+            csb.InitialCatalog = DataBase;
+            csb.IntegratedSecurity = true;
+
+            string connString = csb.ToString();
+
+            //Be sure to replace <YourTable> with the actual name of the Table
+            string queryString = "INSERT INTO [Login Stats](Username, Password, LoggedIn, AccountCreated, LastLogin) VALUES ('"+username+"', '"+password+"', 0, GETDATE(), NULL);";
+            //string queryString = "select * from [Login Stats]";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+
+                connection.Open();
+                //command.ExecuteReader();
+
+                
+
+                
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Send these to your WinForms textboxes
+                        ProfileData PD = new ProfileData();
+                        PD.Username = reader["Username"].ToString();
+                        PD.Password = reader["Password"].ToString();
+                        PD.LoggedIn = Convert.ToBoolean(reader["LoggedIn"].ToString());
+                        Accounts.Add(PD);
+                    }
+                }
+                Console.WriteLine();
+
+
+               /* foreach(ProfileData PD in Accounts)
+                {
+                    if(PD.Username == textBox1.)
+                }
+                */
+            }
+        }
+
+
+
         // Login Button
         private void button1_Click(object sender, EventArgs e)
         {
